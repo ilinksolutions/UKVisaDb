c8e8229a4e3cbb510e598991743cc673022afbb4
Junaid Qureshi <jaq1973@gmail.com>
2019-09-17 15:06:45 -0400

    Update Controller

--- src/main/java/com/ilinksolutions/UKVisaDb/UKVisaDbApplication.java
+++ src/main/java/com/ilinksolutions/UKVisaDb/UKVisaDbApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class P2Application
+public class UKVisaDbApplication
 {
 	public static void main(String[] args)
 	{
-		SpringApplication.run(P2Application.class, args);
+		SpringApplication.run(UKVisaDbApplication.class, args);
 	}
 
 }

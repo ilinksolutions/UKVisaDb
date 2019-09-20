9b99e134463343db5a50bd845352876050e66d9a
Junaid Qureshi <jaq1973@gmail.com>
2019-09-20 10:50:26 -0400

    API Def

--- src/main/java/com/ilinksolutions/UKVisaDb/config/SwaggerConfig.java
+++ src/main/java/com/ilinksolutions/UKVisaDb/config/SwaggerConfig.java
@@ -3,16 +3,16 @@ package com.ilinksolutions.UKVisaDb.config;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import org.springframework.context.annotation.Bean;
+
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
@@ -28,14 +28,19 @@ public class SwaggerConfig {
     }
 
 	    private ApiInfo metaInfo() {
-	    	
-	        return new ApiInfoBuilder().title("UK Visa REST API")
-	            .description("This APIs used to save and update person data.")
-	            .contact(new Contact("I-Link Solutions, Inc.",  "http://ilinksolution.com/", "junaid.qureshi@ilinksolution.com"))
-	            .license("I-Link Solutions, Inc. License Version 2.0")
-	            .licenseUrl("http://ilinksolution.com/")
-	            .version("1.0.0")
-	            .build();
+
+	        ApiInfo apiInfo = new ApiInfo(
+	                "UK Visa API",
+	                "This API used for testing the UK Visa Rest APIs",
+	                "1.0",
+	                "Terms of Service",
+	                new Contact("I-Link Solutions, Inc.", "http://ilinksolution.com/",
+	                        "junaid.qureshi@ilinksolution.com"),
+	                "I-Link Solutions, Inc. License Version 2.0",
+	                "http://ilinksolution.com/", null
+	        );
+
+	        return apiInfo;
 	    }
 	
 }
